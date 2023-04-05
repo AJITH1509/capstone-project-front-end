b2bdd4a2d511442fce0ebc943fae3b05d77c70c0
AJITH1509 <ajithmuthu799@gmail.com>
2023-04-05 09:57:58 +0530

    Loading component added

--- src/ColorPalette.jsx
+++ src/ColorPalette.jsx
@@ -3,9 +3,10 @@ import Card from "@mui/material/Card";
 import { Dashbaord } from "./dashboard.jsx";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import { API } from "../global.js";
+import { Loading } from "./loading.jsx";
 
 export const ColorPalette = () => {
-  const [color, setColor] = useState([]);
+  const [color, setColor] = useState(null);
 
   useEffect(() => {
     fetch(`${API}/colors`, {
@@ -19,11 +20,15 @@ export const ColorPalette = () => {
   return (
     <div>
       <Dashbaord />
-      <div className="color-list">
-        {color?.map((colors) => (
-          <Colors key={colors._id} colors={colors} />
-        ))}
-      </div>
+      {color ? (
+        <div className="color-list">
+          {color.map((colors) => (
+            <Colors key={colors._id} colors={colors} />
+          ))}
+        </div>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
--- src/profile.jsx
+++ src/profile.jsx
@@ -4,6 +4,7 @@ import { API } from "../global.js";
 import Card from "@mui/material/Card";
 import { Dashbaord } from "./dashboard.jsx";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import { Loading } from "./loading.jsx";
 
 export const Profile = () => {
   const [data, setData] = useState("");
@@ -18,12 +19,16 @@ export const Profile = () => {
   return (
     <div>
       <Dashbaord />
-      <Card className="profile-page">
-        <div style={{ borderRadius: "50%" }}>
-          <AccountCircleRoundedIcon sx={{ fontSize: "100px" }} />
-        </div>
-        <h2>{data.name}</h2>
-      </Card>
+      {data ? (
+        <Card className="profile-page">
+          <div style={{ borderRadius: "50%" }}>
+            <AccountCircleRoundedIcon sx={{ fontSize: "100px" }} />
+          </div>
+          <h2>{data.name}</h2>
+        </Card>
+      ) : (
+        <Loading />
+      )}
     </div>
   );
 };
